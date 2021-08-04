16b12bbf6f299350ea285a93097eca046e74075a
dumbstupidbaby <88417738+dumbstupidbaby@users.noreply.github.com>
2021-08-03 23:54:49 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -34,4 +34,5 @@ client.on('message', message =>{
     }
 });
 
-client.login('ODcyMjU0OTcxNjI0Njk3ODY2.YQnMvg.OJZ05e8mKAet-Z6VmBDEdaEkMEk');
+const bufferVariable = require('./secret/secret.js');
+client.login(bufferVariable);
