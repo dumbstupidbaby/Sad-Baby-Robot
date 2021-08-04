3eb0a15ac8d0d987cfe40b5dddafecf2c93bd083
dumbstupidbaby <brenboberto@gmail.com>
2021-08-04 10:17:12 -0500

    hopefully removed .env and made fun of it being a dickhead in .gitignore

--- main.js
+++ main.js
@@ -14,32 +14,4 @@ client.events = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord);
 })
 
-/* const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
-
-    client.commands.set(command.name, command);
-}
-
-//console log when bot is online
-     client.once('ready', () => {
-    console.log('hihihi SADBABYROBOT is online HHIHIIII:D');
-});
-
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    // list commands and stuff
-    if(command === 'ping'){
-        client.commands.get('ping').execute(message, args);
-    }else if(command === 'roll'){
-        client.commands.get('roll').execute(message, args);
-    }else if(command === 'reactionrole'){
-        client.commands.get('reactionrole').execute(message, args, Discord, client);
-    }
-}); */
-
 client.login(process.env.DISCORD_TOKEN);
